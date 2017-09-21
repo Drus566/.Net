4a128b987ae9760e31ca6972c9c6efb8c03ea73b
Drus <ahdpeu566@mail.ru>
2017-09-21 20:06:07 +0300

    3
    3

---
+++ NetworkingProgrammingCSharp/VS2017/PicturePackager/PicturePackager/PicturePackage.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+
+namespace PicturePackager
+{
+    public class PicturePackage
+    {
+        private string name;
+        private int id;
+        private int segmentNumber;
+        private int numberOfSegments;
+        private byte[] segmentBuffer;
+
+        public string Name { get { return name; } }
+        public int Id { get { return id; } }
+        public int SegmentNumber { get { return segmentNumber; } }
+        public int NumberOfSegments { get { return numberOfSegments; } }
+        public byte[] SegmentBuffer { get { return segmentBuffer; } }
+
+        //Конструктор создает сегмент изображения из типов данных 
+        //Используется серверным приложением
+        public PicturePackage(string name, int id, int segmentNumber, int numberOfSegments, byte[] segmentBuffer)
+        {
+            this.name = name;
+            this.id = id;
+            this.segmentNumber = segmentNumber;
+            this.numberOfSegments = numberOfSegments;
+            this.segmentBuffer = segmentBuffer;
+        }
+
+        //Создает сегмент изображения по XML коду
+        //Используется клиентским приложением
+        public PicturePackage(XmlDocument xml)
+        {
+            XmlNode rootNode = xml.SelectSingleNode("PicturePackage");
+            id = int.Parse(rootNode.Attributes["Number"].Value);
+
+            XmlNode nodeName = rootNode.SelectSingleNode("Name");
+            this.name = nodeName.InnerXml;
+
+            XmlNode nodeData = rootNode.SelectSingleNode("Data");
+            numberOfSegments = int.Parse(nodeData.Attributes["SegmentNumber"].Value);
+
+            int size = int.Parse(nodeData.Attributes["Size"].Value);
+            segmentBuffer = Convert.FromBase64String(nodeData.InnerXml);
+
+            //Пример XML документа
+            // <PicturePackage Number = "4">
+            //      <Name>hello.jpg</Name>
+            //      <Data SegmentNumber = "2" LastSegmentNumber = "12" Size = "2400">
+            //          <!- base-64 encoded picture data ->
+            //      </Data>
+            // </PicturePackage>
+        }
+
+        //Возвращаем XML код, представляющий сегмент изображения
+        public string GetXml()
+        {
+            XmlDocument doc = new XmlDocument();
+
+            //Корневой элемент <Picture Package>
+            XmlElement picturePackage = doc.CreateElement("PicturePackage");
+
+            //<PicturePackage.Number="number"></PicturePackage>
+            XmlAttribute pictureNumber = doc.CreateAttribute("Number");
+            pictureNumber.Value = id.ToString();
+            picturePackage.Attributes.Append(pictureNumber);
+
+            //<Name>pictureName</Name>
+            XmlElement pictureName = doc.CreateElement("Name");
+            pictureName.InnerText = name;
+            picturePackage.AppendChild(pictureName);
+
+            //<Data SegmentNumber = "" Size = ""> (фрагмент  в кодировке base-64)
+            XmlElement data = doc.CreateElement("Data");
+            XmlAttribute numberAttr = doc.CreateAttribute("SegmentNumber");
+            numberAttr.Value = segmentNumber.ToString();
+            data.Attributes.Append(numberAttr);
+
+            XmlAttribute lastNumberAttr = doc.CreateAttribute("LastSegmentNumber");
+            lastNumberAttr.Value = numberOfSegments.ToString();
+            data.Attributes.Append(lastNumberAttr);
+
+            data.InnerText = Convert.ToBase64String(segmentBuffer);
+            XmlAttribute sizeAttr = doc.CreateAttribute("Size");
+            sizeAttr.Value = segmentBuffer.Length.ToString();
+            data.Attributes.Append(sizeAttr);
+
+            picturePackage.AppendChild(data);
+
+            doc.AppendChild(picturePackage);
+
+            return doc.InnerXml;
+        }
+    }
+}
---
+++ NetworkingProgrammingCSharp/VS2017/PicturePackager/PicturePackager/PicturePackager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PicturePackager
+{
+    class PicturePackager
+    {
+        protected PicturePackager()
+        {
+        }
+
+        //По завершенному изображению создает сегменты изображения
+        public static PicturePackage[] GetPicturePackages(string name, int id, Image picture)
+        {
+            return GetPicturePackages(name, id, picture);
+        }
+
+        //Возвращает сегменты изображения для завершенного изображения
+        public static PicturePackage[] GetPicturePackages(string name, int id, Image picture, int segmentSize)
+        {
+            //Сохраняем изображение в массиве байтов
+            MemoryStream stream = new MemoryStream();
+            picture.Save(stream, ImageFormat.Jpeg);
+
+            //Вычисляем число сегментов, на которые разбивается изображение
+            int numberSegments = (int)stream.Position / segmentSize + 1;
+
+            PicturePackage[] packages = new PicturePackage[numberSegments];
+
+            //Создаем сегменты изображения
+            int sourceIndex = 0;
+            for(int i = 0; i < numberSegments; i++)
+            {
+                //Вычисляем размер буфера сегмента
+                int bytesToCopy = (int)stream.Position - sourceIndex; 
+                if(bytesToCopy > segmentSize)
+                {
+                    bytesToCopy = segmentSize;
+                }
+                byte[] segmentBuffer = new byte[bytesToCopy];
+                //копируем stream с позиции sourceIndex в segmentBuffer с позиции 0 с длинной bytesToCopy
+                Array.Copy(stream.GetBuffer(), sourceIndex, segmentBuffer, 0, bytesToCopy);
+                packages[i] = new PicturePackage(name, id, i + 1, numberSegments, segmentBuffer);
+                sourceIndex += bytesToCopy;
+            }
+            return packages;
+        }
+
+        //Метод возвращает завершенное изображение из переданных ему сегментов
+        public static Image GetPicture(PicturePackage[] packages)
+        {
+            int fullSizeNeeded = 0;
+            int numberPackages = packages[0].NumberOfSegments;
+            int pictureId = packages[0].Id;
+
+            //Вычисляем размер данных изображения и проверяем согласованность
+            //идентификаторов изображения
+            for(int i = 0; i < numberPackages; i++)
+            {
+                fullSizeNeeded += packages[i].SegmentBuffer.Length;
+                if(packages[i].Id != pictureId)
+                {
+                    throw new ArgumentException("Inconsistent picture ids passed", "packages");
+                }
+            }
+
+            //Объединяем сегменты в двоичный массив
+            byte[] buffer = new byte[fullSizeNeeded];
+            int destinationIndex = 0; 
+            for(int i =0; i < numberPackages; i++)
+            {
+                int length = packages[i].SegmentBuffer.Length;
+                Array.Copy(packages[i].SegmentBuffer, 0, buffer, destinationIndex, length);
+                destinationIndex += length;
+            }
+
+            //Создаем объект image из двоичных данных
+            MemoryStream stream = new MemoryStream(buffer);
+            Image image = Image.FromStream(stream);
+
+            return image;
+        }
+    }
+}
