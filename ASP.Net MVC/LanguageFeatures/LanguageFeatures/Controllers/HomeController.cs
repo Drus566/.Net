dd78281106ace28365738c9f22bdd2af0ee77824
Drus <ahdpeu566@mail.ru>
2017-12-04 14:45:15 +0500

    c# features
    features

---
+++ ASP.Net MVC/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+using System.Linq;
+using System;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() =>
+        View(Product.GetProducts().Select(p => p?.Name));
+    }
+}
+
+
+    /* 
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            Product[] productArray =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+
+            decimal priceFilterTotal = productArray
+                .Filter(p => (p?.Price ?? 0 ) >= 20) // Lambda expression   
+                .TotalPrices();
+            decimal nameFilterTotal = productArray
+                .Filter(p => p?.Name?[0] == 'S')
+                .TotalPrices();
+
+            return View("Index", new string[] {
+                $"Price Total: {priceFilterTotal:C2}",
+                $"Name Total: {nameFilterTotal:C2}" });
+        }
+        */
