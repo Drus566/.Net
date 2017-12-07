7e437d3780ceb83a02b0b3d8647934b1377853b2
Drus <ahdpeu566@mail.ru>
2017-12-07 22:17:54 +0500

    Next Chapter
    VS features

--- ASP.Net MVC/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ ASP.Net MVC/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using LanguageFeatures.Models;
 using System.Linq;
 using System;
+using System.Threading.Tasks;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() =>
-        View(Product.GetProducts().Select(p => p?.Name));
+        public ViewResult Index()
+        {
+            var products = new[] {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+            };
+            return View(products.Select(p => $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
+        }
     }
 }
 
