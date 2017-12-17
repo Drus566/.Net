74ca764acad29ad98fe0083ec2f36a54642c8619
Drus <ahdpeu566@mail.ru>
2017-12-17 14:13:06 +0500

    ds
    s

--- ASP.Net MVC/SportsStore/SportsStore/Controllers/ProductController.cs
+++ ASP.Net MVC/SportsStore/SportsStore/Controllers/ProductController.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 using System.Linq;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
-        private int PageSize = 4;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
@@ -15,10 +16,16 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult List(int productPage = 1)
-            => View(repository.Products
+            => View(new ProductsListViewModel{
+                Products = repository.Products
                 .OrderBy(p => p.ProductID)
                 .Skip((productPage - 1) * PageSize)
-                .Take(PageSize));
-
+                .Take(PageSize),
+            PagingInfo = new PagingInfo{
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            }
+        });   
     }
 }
