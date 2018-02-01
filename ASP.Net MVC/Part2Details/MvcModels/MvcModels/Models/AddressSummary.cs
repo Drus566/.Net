bb4f1457d9d5cc8fc6387e1f631be48ee705d6bb
Drus <ahdpeu566@mail.ru>
2018-02-01 20:36:14 +0500

    Bind Model
    Bind Model Chapter

---
+++ ASP.Net MVC/Part2Details/MvcModels/MvcModels/Models/AddressSummary.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MvcModels.Models
+{
+    public class AddressSummary
+    {
+        //[BindNever]
+        public string City { get; set; }
+
+        public string Country { get; set; }
+    }
+}
